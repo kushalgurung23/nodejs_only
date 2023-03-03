ffa7abdd5467994f20c6e716286d6f5638f539d9
unknown <qoosal23.kg@gmail.com>
2023-03-03 18:52:42 +0800

    ..

---
+++ 12-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    else if(req.url === '/about') {
+        res.end('Welcome to about page')
+    }
+    else {
+        res.end(`<h1>Oops!</h1>
+    <p>Page not found</p>
+    <a href='/'>Back home</a>
+    `);
+    }
+});
+
+server.listen(5000);
--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// 1:28:19
+// npm (node package manager). Two types:
+
+// local dependency - use it only in this particular project
+// npm i <packagename> like expressjs, bootstrap
+
+// glocal dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project and packages)
+// ways of creating:
+// 1. manual approach (create package.json in the root, create properties etc)
+// 2. npm init (step by step, press enter to skip)
+// 3. npm init -y (everything default)
+
+const _ = require('lodash');
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
+
+// 2:02:56
